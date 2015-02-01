7f90a90426cf053ae7f7fd2b6384d2bed1a0fa30
fred <sousseinglu@gmail.com>
2015-02-02 00:13:20 +0100

    Ajout des fichiers Base.java et BaseDistante.java

---
+++ State_Machine/src/main/java/controller/Base.java
@@ -0,0 +1,14 @@
+package controller;
+
+import java.rmi.RemoteException;
+import java.util.*;
+
+//Classe représentant une base de licenciés, présente en interne sur le serveur.
+public class Base {
+	
+	public boolean createStateMachine(String nom) throws RemoteException{
+		
+		return false;
+	}
+
+}
