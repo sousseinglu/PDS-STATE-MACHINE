77c81cd70aa5fdaba7a626cc432eade8c4b899f3
balamuthu1 <balabascarinmuthu@gmail.com>
2015-02-17 19:37:11 +0100

    added registry.lookup added registry.rebind

--- src/main/java/ClassesRMI/ServeurRMI.java
+++ src/main/java/ClassesRMI/ServeurRMI.java
@@ -2,6 +2,8 @@ package ClassesRMI;
 
 
 import java.rmi.*;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 // Classe du Serveur RMI
 public class ServeurRMI {
@@ -11,7 +13,11 @@ public class ServeurRMI {
 			StateMachineDAOExportee statem = new StateMachineDAOExportee();
 			// Puis on enregistre le service associé à la base dans le registry, sous le nom "Base",
 			// pour que les clients puissent le trouver.
-			Naming.rebind("StateMachine", statem);
+			Registry registry = LocateRegistry.createRegistry(10000);
+			registry.rebind("StateMachine", statem);
+			if (System.getSecurityManager() == null){
+		    System.setSecurityManager(new RMISecurityManager());
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
